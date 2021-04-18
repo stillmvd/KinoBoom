348a6cc0becb2266957f9a35dbb121fd96cc55ea
AssLocker <riddimmuffin@gmail.com>
2021-04-18 18:18:37 +0300

    ma biggest work

--- js/main.js
+++ js/main.js
@@ -6,6 +6,180 @@ function fullscreen() {
     if (changeScreen.requestFullscreen) changeScreen.requestFullscreen();
 }
 
-function changeIcon(event) {
-    expandButton.style.backgroundImage = 'url(/img/outscreen.svg)';
+document.addEventListener('fullscreenchange', (event) => {
+    if (document.fullscreenElement) {
+        document.getElementById('fullscreen').style.opacity = '0%';
+    } else {
+        document.getElementById('fullscreen').style.opacity = '100%';
+    }
+  });
+
+function ChangeOver() {
+    document.getElementById('logo').style.transform = 'scale(1.15)';
+    document.getElementById('mainPage').style.marginLeft = '36px';
+}
+    
+function ChangeOut() {   
+    document.getElementById('logo').style.transform = 'scale(1)';
+    document.getElementById('mainPage').style.marginLeft = '0px';
+}
+
+function showInfo() {   
+    document.getElementById('filmInfo').style.opacity = '100%';
+}
+
+function hideInfo() {   
+    document.getElementById('filmInfo').style.opacity = '0%';
+}
+
+function showInfo1() {   
+    document.getElementById('filmInfo1').style.opacity = '100%';
+}
+
+function hideInfo1() {   
+    document.getElementById('filmInfo1').style.opacity = '0%';
+}
+
+function showInfo2() {   
+    document.getElementById('filmInfo2').style.opacity = '100%';
+}
+
+function hideInfo2() {   
+    document.getElementById('filmInfo2').style.opacity = '0%';
+}
+
+function showInfo3() {   
+    document.getElementById('filmInfo3').style.opacity = '100%';
+}
+
+function hideInfo3() {   
+    document.getElementById('filmInfo3').style.opacity = '0%';
+}
+
+function showInfo4() {   
+    document.getElementById('filmInfo4').style.opacity = '100%';
+}
+
+function hideInfo4() {   
+    document.getElementById('filmInfo4').style.opacity = '0%';
+}
+
+function showInfo5() {   
+    document.getElementById('filmInfo5').style.opacity = '100%';
+}
+
+function hideInfo5() {   
+    document.getElementById('filmInfo5').style.opacity = '0%';
+}
+
+function showInfo6() {   
+    document.getElementById('filmInfo6').style.opacity = '100%';
+}
+
+function hideInfo6() {   
+    document.getElementById('filmInfo6').style.opacity = '0%';
+}
+
+function showInfo7() {   
+    document.getElementById('filmInfo7').style.opacity = '100%';
+}
+
+function hideInfo7() {   
+    document.getElementById('filmInfo7').style.opacity = '0%';
+}
+
+function showInfo8() {   
+    document.getElementById('filmInfo8').style.opacity = '100%';
+}
+
+function hideInfo8() {   
+    document.getElementById('filmInfo8').style.opacity = '0%';
+}
+
+function showInfo9() {   
+    document.getElementById('filmInfo9').style.opacity = '100%';
+}
+
+function hideInfo9() {   
+    document.getElementById('filmInfo9').style.opacity = '0%';
+}
+
+function showInfo10() {   
+    document.getElementById('filmInfo10').style.opacity = '100%';
+}
+
+function hideInfo10() {   
+    document.getElementById('filmInfo10').style.opacity = '0%';
+}
+
+function showInfo11() {   
+    document.getElementById('filmInfo11').style.opacity = '100%';
+}
+
+function hideInfo11() {   
+    document.getElementById('filmInfo11').style.opacity = '0%';
+}
+
+function showInfo12() {   
+    document.getElementById('filmInfo12').style.opacity = '100%';
+}
+
+function hideInfo12() {   
+    document.getElementById('filmInfo12').style.opacity = '0%';
+}
+
+function showInfo13() {   
+    document.getElementById('filmInfo13').style.opacity = '100%';
+}
+
+function hideInfo13() {   
+    document.getElementById('filmInfo13').style.opacity = '0%';
+}
+
+function showInfo14() {   
+    document.getElementById('filmInfo14').style.opacity = '100%';
+}
+
+function hideInfo14() {   
+    document.getElementById('filmInfo14').style.opacity = '0%';
+}
+
+function showInfo15() {   
+    document.getElementById('filmInfo15').style.opacity = '100%';
+}
+
+function hideInfo15() {   
+    document.getElementById('filmInfo15').style.opacity = '0%';
+}
+
+function showInfo16() {   
+    document.getElementById('filmInfo16').style.opacity = '100%';
+}
+
+function hideInfo16() {   
+    document.getElementById('filmInfo16').style.opacity = '0%';
+}
+
+function showInfo17() {   
+    document.getElementById('filmInfo17').style.opacity = '100%';
+}
+
+function hideInfo17() {   
+    document.getElementById('filmInfo17').style.opacity = '0%';
+}
+
+function showInfo18() {   
+    document.getElementById('filmInfo18').style.opacity = '100%';
+}
+
+function hideInfo18() {   
+    document.getElementById('filmInfo18').style.opacity = '0%';
+}
+
+function showInfo19() {   
+    document.getElementById('filmInfo19').style.opacity = '100%';
+}
+
+function hideInfo19() {   
+    document.getElementById('filmInfo19').style.opacity = '0%';
 }
